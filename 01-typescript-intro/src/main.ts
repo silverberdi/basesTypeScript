c1af6d706990d391c3d82d991f9ed266394d693d
Silverio Bernal <silverio.bernal@gmail.com>
2026-03-05 11:20:53 -0500

    02-object-interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import './topics/01-basic-types';
+//import './topics/01-basic-types';
+import './topics/02-object-interfaces';
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
